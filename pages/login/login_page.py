1df41b229cc6ac31e75e2a4ca1e2a8b8ac405428
aravindanath <aravindanath86@gmail.com>
2019-06-12 05:59:48 +0530

    update Login test

--- pages/login/login_page.py
+++ pages/login/login_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from base.selenium_driver import SeleniumDriver
+import time
+
 class LoginPage(SeleniumDriver):
  
     # Constructor will accept driver
@@ -9,52 +11,65 @@ class LoginPage(SeleniumDriver):
         self.driver = driver
 
     # Locators
-    _sign_in_link = "Hello, Sign in"
+    _search_id = "twotabsearchtextbox"
+    _sign_in_xpath = "//*[@id='nav-link-accountList']/span[1]"
     _email_field = "ap_email"
     _pwd_field = "ap_password"
     _continue_button = "continue"
     _login_button ="signInSubmit"
 
-    def getSignIn(self):
-        return self.find_element(By.LINK_TEXT,self._sign_in_link)
-
-    def getEmail(self):
-        return self.find_element(By.ID, self._email_field)
-
-    def getPassword(self):
-        return self.find_element(By.ID, self._pwd_field)
-
-    def getContinueBtn(self):
-        return self.find_element(By.ID, self._continue_button)
-
-    def getLoginBtn(self):
-        return self.find_element(By.ID, self._login_button)
+    # def getSignIn(self):
+    #     return self.find_element(By.LINK_TEXT,self._sign_in_link)
+    #
+    # def getEmail(self):
+    #     return self.find_element(By.ID, self._email_field)
+    #
+    # def getPassword(self):
+    #     return self.find_element(By.ID, self._pwd_field)
+    #
+    # def getContinueBtn(self):
+    #     return self.find_element(By.ID, self._continue_button)
+    #
+    # def getLoginBtn(self):
+    #     return self.find_element(By.ID, self._login_button)
 
     # Actions
+    def search(self,data):
+        self.sendKeys(data,self._search_id,locatorType="id")
+
 
     def clickSigninLink(self):
-        self.getSignIn().click()
+        self.elementClick(self._sign_in_xpath,locatorType="xpath")
+
 
     def enterEmail(self,email):
-        self.getEmail().send_keys(email)
+        self.sendKeys(email,self._email_field,locatorType="id")
+
       
     def clickContinueBtn(self):
-        self.getContinueBtn().click()
+        self.elementClick(self._continue_button,locatorType="id")
+
 
     def enterPassword(self, pwd):
-        self.getPassword().send_keys(pwd)
+        self.sendKeys(pwd, self._pwd_field, locatorType="id")
+
 
     def clickLoginBtn(self):
-        self.getLoginBtn().click()
+        self.elementClick(self._login_button,locatorType="id")
 
 
 
-    def login(self,email,password):
-        self.clickSigninLink()
-        self.enterEmal(email)
+    def login(self,data,email,password):
+
+        # self.clickSigninLink()
+
+        self.enterEmail(email)
         self.clickContinueBtn()
+        self.take_Screenshot(driver=self.driver)
         self.enterPassword(password)
         self.clickLoginBtn()
-
+        time.sleep(5)
+        self.search(data)
+        time.sleep(5)
 
 
